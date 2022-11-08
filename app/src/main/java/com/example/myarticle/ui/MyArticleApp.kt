2ca1a2c44e395b791212200e6c38252b2db31fda
mtnmr <mei.rila1013@gmail.com>
2022-11-08 11:44:43 +0900

    save article dialog

--- app/src/main/java/com/example/myarticle/ui/MyArticleApp.kt
+++ app/src/main/java/com/example/myarticle/ui/MyArticleApp.kt
@@ -6,15 +6,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myarticle.R
 import com.example.myarticle.model.Article
 import com.example.myarticle.model.sampleData
 import com.example.myarticle.ui.theme.MyArticleTheme
@@ -26,14 +27,12 @@ fun MyArticleApp(
     viewModel: ArticleViewModel = viewModel()
 ){
     val articleList by viewModel.allArticles.collectAsState()
+    var openDialog by remember { mutableStateOf(false) }
 
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
-                onClick = {
-                    val article = Article(title = "sample", link = "https://sample.link", date = Date())
-                    viewModel.insertArticle(article)
-                }
+                onClick = { openDialog = true }
             ) {
                 Icon(Icons.Default.Add, contentDescription = "add article button")
             }
@@ -43,6 +42,13 @@ fun MyArticleApp(
             articleList = articleList,
             modifier = Modifier.padding(innerPadding)
         )
+
+        if(openDialog){
+            SaveDialog(
+                onSaveButtonClicked = { openDialog = false },
+                onCancelButtonClicked = { openDialog = false }
+            )
+        }
     }
 }
 
@@ -55,7 +61,10 @@ fun ArticleList(
         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ){
-        items(articleList){ article ->
+        items(
+            articleList,
+            key =  {item: Article ->  item.id}
+        ){ article ->
             ArticleListItem(article = article)
         }
     }
@@ -93,7 +102,93 @@ fun ArticleListItem(
                 )
             }
         }
+    }
+}
 
+@Composable
+fun SaveAlertDialog(
+    onSaveButtonClicked:() -> Unit,
+    onCancelButtonClicked:() -> Unit
+){
+    AlertDialog(
+        onDismissRequest = {  },
+        title = {
+            Text(text = stringResource(id = R.string.save_dialog_title))
+        },
+        text ={
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "title") }
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onSaveButtonClicked
+            ) {
+                Text(text = stringResource(id = R.string.save_article))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onCancelButtonClicked
+            ) {
+                Text(text = stringResource(id = R.string.save_article_cancel))
+            }
+        }
+    )
+}
+
+@Composable
+fun SaveDialog(
+    onSaveButtonClicked:() -> Unit,
+    onCancelButtonClicked:() -> Unit
+){
+    var title by remember { mutableStateOf("") }
+    var link by remember { mutableStateOf("") }
+
+    Dialog(onDismissRequest = {  }) {
+        Surface() {
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ){
+                Text(
+                    text = stringResource(id = R.string.save_dialog_title),
+                )
+
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = { Text(text = stringResource(id = R.string.save_article_title))},
+                    modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+                )
+
+                OutlinedTextField(
+                    value = link,
+                    onValueChange = { link = it },
+                    label = { Text(text = stringResource(id = R.string.save_article_link))},
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(
+                        onClick = onCancelButtonClicked,
+                    ) {
+                        Text(text = stringResource(id = R.string.save_article_cancel))
+                    }
+
+                    TextButton(
+                        onClick = onSaveButtonClicked,
+                        modifier = Modifier.padding(start = 8.dp)
+                    ) {
+                        Text(text = stringResource(id = R.string.save_article))
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -119,6 +214,38 @@ fun ArticleListPreview(){
             color = MaterialTheme.colors.background
         ) {
             ArticleList(articleList = sampleData)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SaveAlertDialogPreview(){
+    MyArticleTheme() {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            SaveAlertDialog(
+                onSaveButtonClicked = {},
+                onCancelButtonClicked = {},
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SaveDialogPreview(){
+    MyArticleTheme() {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            SaveDialog(
+                onSaveButtonClicked = {},
+                onCancelButtonClicked = {},
+            )
         }
     }
 }
